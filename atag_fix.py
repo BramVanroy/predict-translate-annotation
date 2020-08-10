73d8c3f6560f70d2034a5b8c9be02b13aee7fadc
Bram Vanroy <Bram.Vanroy@UGent.be>
2020-08-10 12:27:42 +0200

    improve documentation

--- atag_fix.py
+++ atag_fix.py
@@ -1,8 +1,8 @@
-"""Fixes atag XML files after manually changing tokenization. Particularly, this script fixes the cur and id attributes.
-   After modifying the manually edited files, it will check whether the number of characters (i.e. cursor positions)
-   is the same between the new file and the original file.
+"""Fixes atag XML files after manually changing tokenization. Particularly, this script fixes the `id` of words
+   to match the modifed ids that the words received after running the fix_tokenization.py script.
+   *Never run this script on the same data twice. That will throw errors!* Only want to run it over a few files
+    that you edited? Copy those files to a separate directory, and use that directory for this script.
    Make sure to back up your data before using this script!"""
-
 from copy import deepcopy
 from pathlib import Path
 from typing import Dict
@@ -84,6 +84,9 @@ class AtagFixer:
 
     def fix_atag_alignments(self):
         """Main entry point to fix the atag files."""
+        n_processed = 0
+        all_valid = True
+        has_error = False
         for identifier, sents_d in self.sents_orig.items():
             if identifier not in self.sents_man:
                 print(f"File {identifier} not found in manual correction. Skipping...")
@@ -103,11 +106,28 @@ class AtagFixer:
                         self.elements_equal(tgt_orig_tree, tgt_man_tree):
                     valid_src_segs.append(src_seg_id)
                 else:
-                    print(f"WARNING: NOT identical for {identifier}, aligned pair {src_seg_id}-{tgt_seg_id}."
-                          f" After uploading files to TPR-DB/YAWAT, this sentence pair will need to be re-aligned.")
+                    print(f"WARNING: NOT identical for {identifier}, aligned pair {src_seg_id}-{tgt_seg_id}.")
+                    all_valid = False
 
             if valid_src_segs:
-                self.update_atag(identifier, valid_src_segs)
+                error = self.update_atag(identifier, valid_src_segs)
+                if error:
+                    has_error = True
+                n_processed += 1
+
+        if has_error:
+            print(f"\nThe 'possible errors' that occurred likely happen because you ran this script twice on the same"
+                  f" data. Once an atag file has been fixed, trying to fix it again will lead to errors. In most cases"
+                  f" you can ignore those warnings. In the future, though, it is best to only run the script only once"
+                  f" over the same files. Only want to run it over a few files that you edited? Copy those files to a"
+                  f" separate directory, and use that directory for this script.")
+
+        if not all_valid:
+            print(f"\nAfter uploading files to TPR-DB/YAWAT, the 'warning' sentence pairs above will need to be"
+                  f" re-aligned. If they are also part of a 'possible errors' warning, there are underlying issues"
+                  f" and they were not processed.")
+
+        print(f"Finished processing {n_processed:,} .atag file(s).")
 
     @classmethod
     def from_dirs(cls, dir_orig, dir_man):
@@ -166,6 +186,7 @@ class AtagFixer:
         align_keys = tree.findall(".//alignFile")
         a_keyfiles = {Path(el.get("href")).suffix.replace(".", ""): el.get("key") for el in align_keys}
 
+        has_error = False
         # For a given segment pair, find the mappings of their tokens
         for src_seg_id in valid_segs:
             tgt_seg_id = self.sent_aligns[identifier][src_seg_id]
@@ -177,9 +198,18 @@ class AtagFixer:
                 src_xml_id = f"{a_keyfiles['src']}{orig_src_token_idx}"
 
                 for el in tree.findall(f".//align[@out='{src_xml_id}']"):
-                    el.set("out", f"{a_keyfiles['src']}{man_src_token_idx}")
-                    prev_out = el.get("in")[1:]
-                    el.set("in", f"{a_keyfiles['tgt']}{idx_maps['tgt'][prev_out]}")
+                    try:
+                        el.set("out", f"{a_keyfiles['src']}{man_src_token_idx}")
+                        prev_out = el.get("in")[1:]
+                        el.set("in", f"{a_keyfiles['tgt']}{idx_maps['tgt'][prev_out]}")
+                    except KeyError:
+                        has_error = True
+                        break
+
+                    if has_error:
+                        print(f"POSSIBLE ERROR: an error occurred for {identifier}. Skipping file...")
+                        return has_error
+
                     valid_tree_root.append(el)
                     # need to remove from original tree to prevent recursion problems
                     # if we change index of a node, it will be found by the query for that new index
@@ -191,6 +221,8 @@ class AtagFixer:
                                                                        int(node.get("out", "a0")[1:])))
 
         valid_tree.write(pfin)
+
+        return has_error
 
 
 if __name__ == '__main__':
